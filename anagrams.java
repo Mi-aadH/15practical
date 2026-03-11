b2fcdd91b2fce1f790661f582c934172d0050eb9
Mi-aadH <4353521@myuwc.ac.za>
2026-03-11 14:42:12 +0200

    Update anagrams.java

--- anagrams.java
+++ anagrams.java
@@ -1,1 +1,12 @@
 //start anagrams project
+
+import java.util.Arrays;
+
+public class Anagrams {
+//creating a signature
+    public static String signature(String word){
+        char [] letters = word.toCharArray();
+        Arrays.sort(letters);
+        return new String(letters);
+    }
+}
